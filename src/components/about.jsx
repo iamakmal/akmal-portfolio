01b0c88be30b24f83f758c20b18502b695883453
iamakmal <m.akmalalavi@gmail.com>
2023-07-05 11:50:54 +0530

    about updated with grid

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
+import {Typography} from '@mui/material'
+import {Box} from '@mui/material'
+import { Grid } from '@mui/material'
+import profilePic from '../assets/profile.jpg'
 
 function about() {
   return (
-    <>
-    <div>About Me</div>
-    <div>A dedicated, hardworking and an enthusiastic individual,
-    looking forward to work in the IT industry. Seeking an
-    internship on Software Development in a well reputed
-    company to utilize my skills and to gain some real world
-    experience in a challenging and motivating working
-    environment. Currently I am an undergraduate at SLIIT,
-    following BSc in Information Technology (Specializing in
-    Software Engineering) degree program. Curiosity to learn
-    new things is what keeps me motivated. I believe that I will
-    be a valuable asset to your company.</div>
-    </>
+    <section id="about">
+    <Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      justifyContent: 'center', 
+    }}
+    >
+    <Typography variant="h1">About Me</Typography>
+    <Grid 
+      container
+      spacing={2}
+      >
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h4">A dedicated, hardworking and an enthusiastic individual,
+        looking forward to work in the IT industry. Seeking an
+        internship on Software Development in a well reputed
+        company to utilize my skills and to gain some real world
+        experience in a challenging and motivating working
+        environment. Currently I am an undergraduate at SLIIT,
+        following BSc in Information Technology (Specializing in
+        Software Engineering) degree program. Curiosity to learn
+        new things is what keeps me motivated. I believe that I will
+        be a valuable asset to your company.</Typography>
+      </Grid>
+
+      <Grid item xs={12} sm={6}>
+        <img src={profilePic} width="50%"/>
+      </Grid>
+
+      </Grid>
+    </Box>
+    </section>
   )
 }
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,44 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Typewriter from 'typewriter-effect'
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 function home() {
   return (
-    <center>
-    <h6>Hi There,</h6>
-    <h1>I am Akmal</h1>
-    <h2>I am an Undergraduate in SLIIT and a Web Developer</h2>
+    <Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      justifyContent: 'center', 
+    }}
+    >
+    <section id="home">
+    <div>
+    <Typography variant="h6">Hi There,</Typography>
+    <Typography variant="h1">I am Akmal</Typography>
+    <Typography variant="h3">
+    <Typewriter
+      options={{
+        strings: [
+          "Undergraduate",
+          "Full Stack Web Developer",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+      />
+    </Typography>
     <a href="https://www.linkedin.com/in/akmal-alavi/"><LinkedInIcon/></a>
     <a href="https://github.com/iamakmal"><GitHubIcon/></a>
     <a href="https://twitter.com/_iamakmal_"><TwitterIcon/></a>
-    </center>
+    </div>
+    </section>
+    </Box>
   )
 }
 
