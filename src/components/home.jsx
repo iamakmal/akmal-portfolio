ff1ea1f8326a41e9ee6f459812befa1ca17ea85b
IT21292422 <it21292422@my.sliit.lk>
2024-02-10 21:37:41 +0530

    home updated

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -23,7 +23,7 @@ function Home() {
     }}
     >
     
-    <Typography variant="h6"align="center">Hi There 👋,</Typography>
+    <Typography variant="h6"align="center">Hello There 👋,</Typography>
     <Typography variant="h1" align="center">I am Akmal</Typography>
     <Typography variant="h3" align="center">
     <Typewriter
